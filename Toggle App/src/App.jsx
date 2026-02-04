6454cbb70a7a57e21217777165279bdacca16437
Syed Ahmed Hussain Ulvaan <hussain.ulvaan@gmail.com>
2026-02-04 16:03:59 +0530

    Add files via upload

---
+++ Toggle App/src/App.jsx
@@ -0,0 +1,9 @@
+import ThemeToggle from "./components/ThemeToggle";
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ThemeToggle />
+    </div>
+  );
+}
